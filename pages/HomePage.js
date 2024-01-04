5b0e204aa8a8bee34f8acd900375d5c01f03ee1e
generiblora <g.riblora@arcanys.com>
2024-01-04 09:41:54 +0800

    add the first testcafe project

---
+++ pages/HomePage.js
@@ -0,0 +1,28 @@
+import { Selector,t,ClientFunction } from 'testcafe';
+
+class HomePage{
+    constructor() {
+        this.subtitleHeader = Selector('h2').withText('Welcome to our store')
+        this.RegisterLink = Selector('a').withText('Register')
+        this.LoginLink = Selector('a').withText('Log in')
+        this.CartLink = Selector('a').withText('Shopping cart')
+        this.MyAccountLink = Selector('a').withText('My account')
+        this.LogoutLink = Selector('a').withText('Log out');
+        this.currencyList = Selector("input[id='customerCurrency']")
+      }
+      get productSearch() { return Selector("input[id='small-searchterms']"); } 
+      
+      async search(product) {
+        await t.
+        typeText(this.productSearch, product).
+        wait(3000).
+        pressKey('enter')
+    }
+    
+      async changeCurrency(currency){
+        await t
+        .click('select#customerCurrency')
+        .click(Selector('option', { text: currency }));
+    }
+}
+export default new HomePage();
